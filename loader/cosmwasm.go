fddcccfcda856f83f4992b50ecdb327e0efbfd00
fdymylja <frojdi.dymylja@gmail.com>
2021-12-22 17:54:17 +0100

    change: remove floats from interface equality

--- loader/cosmwasm.go
+++
@@ -1,7 +0,0 @@
-package loader
-
-// mergeCosmwasmPaths overrides packages that need to be modified in order to support
-// cosmwasm - specifically it overrides packages that contain operations that involve floats.
-func mergeCosmwasmPaths(m map[string]bool) {
-	m["strconv/"] = true
-}
